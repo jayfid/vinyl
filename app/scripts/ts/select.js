197a1e845a3f94fb8e3aff3e6c770d018ac2819f
jayfid <j.fidlow@gmail.com>
2016-08-31 23:42:27 -0400

    @WIP

--- app/scripts/ts/select.js
+++
@@ -1,9 +0,0 @@
-var VinylFormSelect = (function () {
-    function VinylFormSelect() {
-    }
-    return VinylFormSelect;
-})();
-window.onload = function () {
-    new VinylFormSelect();
-};
-//# sourceMappingURL=select.js.map
