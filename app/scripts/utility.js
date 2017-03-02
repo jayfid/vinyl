2f3f4fa9423fbaba7d3ccd1ec5c8cc7a48296233
jayfid <j.fidlow@gmail.com>
2017-03-02 02:10:44 -0500

    added docs to draggable and utility methods

--- app/scripts/utility.js
+++ app/scripts/utility.js
@@ -5,16 +5,17 @@
 function VinylUtil() {
     return this;
 }
+
 /**
  * Check if image failed to load.
  * @param  {Element} img - An <img> node.
  * @return {Boolean} True if image is loaded, otherwise false.
  */
-VinylUtil.prototype.isImageOk = function(img) {
+VinylUtil.prototype.isImageOk = function (img) {
     // adapted from https://stereochro.me/ideas/detecting-broken-images-js
-    if (!img.complete
-        || (typeof img.naturalWidth !== 'undefined'
-            && img.naturalWidth === 0)) {
+    if (!img.complete ||
+        (typeof img.naturalWidth !== 'undefined' &&
+            img.naturalWidth === 0)) {
         return false;
     }
     return true;
@@ -26,7 +27,7 @@ VinylUtil.prototype.isImageOk = function(img) {
  * @param  {String} elemClass - Class to toggle.
  * @return {Boolean} True on success, otherwise false.
  */
-VinylUtil.prototype.toggleClass = function(element, elemClass) {
+VinylUtil.prototype.toggleClass = function (element, elemClass) {
     if (!element || !elemClass) {
         return false;
     }
@@ -43,7 +44,7 @@ VinylUtil.prototype.toggleClass = function(element, elemClass) {
  * @param  {String} elemClass - Class name to match.
  * @return {Boolean} True if element has class, otherwise false.
  */
-VinylUtil.prototype.hasClass = function(element, elemClass) {
+VinylUtil.prototype.hasClass = function (element, elemClass) {
     if (typeof element.className !== 'string') {
         return false;
     }
@@ -77,7 +78,7 @@ VinylUtil.prototype.hasClass = function(element, elemClass) {
  * @param  {String} elemClass - Class to add to element.
  * @return {Boolean} True on success, otherwise false.
  */
-VinylUtil.prototype.addClass = function(element, elemClass) {
+VinylUtil.prototype.addClass = function (element, elemClass) {
     if (typeof element.className === 'undefined') {
         return false;
     }
@@ -97,7 +98,7 @@ VinylUtil.prototype.addClass = function(element, elemClass) {
  * @param  {Element} referencedElement - DOM element whose height will be read.
  * @return {Boolean} Always returns true.
  */
-VinylUtil.prototype.setHeightOnElement = function(element, referencedElement) {
+VinylUtil.prototype.setHeightOnElement = function (element, referencedElement) {
     element.style.height = referencedElement.offsetHeight + 'px';
     return true;
 };
@@ -108,7 +109,7 @@ VinylUtil.prototype.setHeightOnElement = function(element, referencedElement) {
  * @param  {String} elemClass - Class to remove from element.
  * @return {Boolean} True on success, otherwise false.
  */
-VinylUtil.prototype.removeClass = function(element, elemClass) {
+VinylUtil.prototype.removeClass = function (element, elemClass) {
     if (typeof element.className === 'undefined') {
         return false;
     }
@@ -136,8 +137,9 @@ VinylUtil.prototype.removeClass = function(element, elemClass) {
  * @param  {Integer} limit - Maximum number of nodes to check
  * @return {Element} The matched element if found, otherwise false.
  */
-VinylUtil.prototype.findParentWithClass = function(element, elemClass, limit) {
-    var found = false, count = 0;
+VinylUtil.prototype.findParentWithClass = function (element, elemClass, limit) {
+    var found = false,
+        count = 0;
     if (!limit) {
         limit = 100;
     }
@@ -164,8 +166,9 @@ VinylUtil.prototype.findParentWithClass = function(element, elemClass, limit) {
  * @param  {Integer} limit - Maximum number of nodes to check
  * @return {Element} The matched element if found, otherwise false.
  */
-VinylUtil.prototype.findParentLink = function(element, limit) {
-    var found = false, count = 0;
+VinylUtil.prototype.findParentLink = function (element, limit) {
+    var found = false,
+        count = 0;
     if (!limit) {
         limit = 100;
     }
@@ -189,7 +192,7 @@ VinylUtil.prototype.findParentLink = function(element, limit) {
  * @param  {Element} element - DOM element.
  * @return {Element} The html element if found, otherwise element.firstChild.
  */
-VinylUtil.prototype.findFirstChild = function(element) {
+VinylUtil.prototype.findFirstChild = function (element) {
     var firstChild = element.firstChild;
     while (firstChild && firstChild.nodeType !== 1) {
         firstChild = firstChild.nextSibling;
@@ -202,7 +205,7 @@ VinylUtil.prototype.findFirstChild = function(element) {
  * @param  {String} key - The get parameter to search for.
  * @return {String} The value of the supplied GET param, otherwise false.
  */
-VinylUtil.prototype.getParam = function(key) {
+VinylUtil.prototype.getParam = function (key) {
     var search = window.location.search.toLowerCase().substring(1),
         params = [];
 
@@ -223,10 +226,16 @@ VinylUtil.prototype.getParam = function(key) {
 }
 
 /**
- * 
+ * Check whether a GET param has the given key
+ * @param {string} pair - the get param, format: paramName=paramValue
+ * @param {string} key - the string to match against the parametere name
+ * @returns {Boolean} true if `pair`'s name equals `key`, otherwise false
  */
-VinylUtil.prototype.compareKeyValuePair = function(pair, key) {
+VinylUtil.prototype.compareKeyValuePair = function (pair, key) {
     var keyValue = pair.split('=');
+    if (keyValue.length !== 2) {
+        return false;
+    }
     var decodedKey = decodeURIComponent(keyValue[0]),
         decodedValue = decodeURIComponent(keyValue[1]);
 
@@ -237,22 +246,32 @@ VinylUtil.prototype.compareKeyValuePair = function(pair, key) {
 };
 
 /**
- * 
+ * Scroll the page to the top or bottom of the passed element.
+ * @param {HTMLElement} elem - element to scroll to.
+ * @param {string} [position=top] - top: top of window = top of element.  bottom: bottom of window = bottom of element.
  */
-VinylUtil.prototype.scrollIntoView = function(elem, position) {
+VinylUtil.prototype.scrollIntoView = function (elem, position) {
+    if (!position) {
+        position = 'top';
+    }
     var currentWindowYOffset = (window.pageYOffset || document.documentElement.scrollTop) -
-        (document.documentElement.clientTop || 0);
-    var elementWindowYOffset;
-    if (position && position === 'bottom') {
-        var elemY = VinylUtil.getPosition(elem).y + currentWindowYOffset;
-        var elemHeight = elem.offsetHeight;
-        var elemBottomPixel = elemY + elemHeight;
-        var extraSpacePixels = 30;
-        var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
-        var calculatedOffset = elemBottomPixel - windowHeight + extraSpacePixels;
-        elementWindowYOffset = (calculatedOffset >= 0) ? calculatedOffset : 0;
-    } else {
-        elementWindowYOffset = VinylUtil.getPosition(elem).y;
+        (document.documentElement.clientTop || 0),
+        elementWindowYOffset;
+    switch (position) {
+        case 'bottom':
+            var elemY = VinylUtil.getPosition(elem).y + currentWindowYOffset,
+                elemHeight = elem.offsetHeight,
+                elemBottomPixel = elemY + elemHeight,
+                extraSpacePixels = 30,
+                windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight,
+                calculatedOffset = elemBottomPixel - windowHeight + extraSpacePixels;
+            elementWindowYOffset = (calculatedOffset >= 0) ? calculatedOffset : 0;
+            break;
+        case 'top':
+            elementWindowYOffset = VinylUtil.getPosition(elem).y;
+            break;
+        default:
+            throw 'Invalid position.';
     }
 
     var initialWindowYOffset = currentWindowYOffset;
@@ -261,7 +280,7 @@ VinylUtil.prototype.scrollIntoView = function(elem, position) {
         return;
     }
 
-    var interval = window.setInterval( function(){
+    var interval = window.setInterval(function () {
         if (currentWindowYOffset !== elementWindowYOffset) {
             var offset = 1;
             var distance = Math.abs(currentWindowYOffset - elementWindowYOffset);
@@ -288,8 +307,8 @@ VinylUtil.prototype.scrollIntoView = function(elem, position) {
  * @param {HTMLElement} elemSelector - CSS selector of element to wait to wait for
  * @param {function} callback
  */
-VinylUtil.prototype.waitUntilVisible = function(elemSelector, callback) {
-    var interval = window.setInterval(function(){
+VinylUtil.prototype.waitUntilVisible = function (elemSelector, callback) {
+    var interval = window.setInterval(function () {
         if (!VinylUtil.checkVisible(document.querySelector(elemSelector))) {
             return;
         }
@@ -300,10 +319,14 @@ VinylUtil.prototype.waitUntilVisible = function(elemSelector, callback) {
 };
 
 /**
+ * Get [x,y] of top left of element.
  * @credit adapted from http://stackoverflow.com/a/24829409
+ * @param {HTMLElement} element
+ * @returns {Integer[]} [x,y] of element.
  */
-VinylUtil.prototype.getPosition = function(element) {
-    var xPosition = 0, yPosition = 0;
+VinylUtil.prototype.getPosition = function (element) {
+    var xPosition = 0,
+        yPosition = 0;
 
     while (element) {
         xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
@@ -318,9 +341,12 @@ VinylUtil.prototype.getPosition = function(element) {
 };
 
 /**
+ * Check if element is on screen
  * @credit adapted from - http://stackoverflow.com/a/5354536
+ * @param {HTMLElement} element
+ * @returns {Boolean} true if element on screen, otherwise false.
  */
-VinylUtil.prototype.checkVisible = function(element) {
+VinylUtil.prototype.checkVisible = function (element) {
     if (!element) {
         throw 'Invalid Element';
     }
@@ -330,9 +356,11 @@ VinylUtil.prototype.checkVisible = function(element) {
 };
 
 /**
+ * Check if device is likely iOS
  * @credit adapted from - http://stackoverflow.com/a/9039885
+ * @returns {Boolean}
  */
-VinylUtil.prototype.iOS = function() {
+VinylUtil.prototype.iOS = function () {
     var iDevices = [
         'iPad Simulator',
         'iPhone Simulator',
@@ -343,14 +371,17 @@ VinylUtil.prototype.iOS = function() {
     ];
     if (navigator.platform) {
         while (iDevices.length) {
-            if (navigator.platform === iDevices.pop()) { return true; }
+            if (navigator.platform === iDevices.pop()) {
+                return true;
+            }
         }
     }
     return false;
 };
 
 /**
- *
+ * Check if browser is likely Safari
+ * @returns {Boolean}
  */
 VinylUtil.prototype.isSafari = function () {
     var e = navigator.userAgent.toLowerCase();
@@ -358,12 +389,14 @@ VinylUtil.prototype.isSafari = function () {
 };
 
 /**
- *
+ * Gets the pointer position from an event.
+ * @param {Event} mouseEvent
+ * @return {integer[]} [x,y] of pointer device.
  */
-VinylUtil.prototype.getMouseCoordinates = function(mouseEvent) {
-    if ('undefined' !== typeof mouseEvent.pageX && 
+VinylUtil.prototype.getMouseCoordinates = function (mouseEvent) {
+    if ('undefined' !== typeof mouseEvent.pageX &&
         'undefined' !== typeof mouseEvent.pageY) {
-        
+
         return [mouseEvent.pageX, mouseEvent.pageY];
     }
     if (mouseEvent.touches && mouseEvent.touches.length) {
@@ -373,17 +406,22 @@ VinylUtil.prototype.getMouseCoordinates = function(mouseEvent) {
 };
 
 /**
- *
+ * Shorthand to add the same callbacks to the same element for multiple events.
+ * Useful for handling redundancy from supporting multiple device types.
+ * @param {string[]} events - array of events
+ * @param {HTMLElement} element - element to which the events are attached.
+ * @param {function} callback
+ * @param {Boolean} [useCapture=false] - @see EventTarget.addEventListener()
  */
-VinylUtil.prototype.addEvents = function(events, element, listener, useCapture) {
-  if (typeof useCapture === 'undefined') {
-    useCapture = false;
-  }
-  for (var i = 0, len = events.length; i < len; i++) {
-    element.addEventListener(events[i], listener, useCapture);
-  }
+VinylUtil.prototype.addEvents = function (events, element, callback, useCapture) {
+    if (typeof useCapture === 'undefined') {
+        useCapture = false;
+    }
+    for (var i = 0, len = events.length; i < len; i++) {
+        element.addEventListener(events[i], callback, useCapture);
+    }
 };
 
-(function() {
-  window.VinylUtil = new VinylUtil();
+(function () {
+    window.VinylUtil = new VinylUtil();
 })();
