b381b6665157ee34e4ab3fe8924b6b368596e5f9
jayfid <j.fidlow@gmail.com>
2017-03-28 14:15:46 -0400

    modified amd loading

--- app/scripts/invoke.js
+++ app/scripts/invoke.js
@@ -1,11 +1,13 @@
 /* global define, Vinylsiding */
 (function (root, factory) {
+    root = root || window;
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['b'], factory);
+        define(function () {
+            return (root.VS = factory());
+        });
     } else {
         // Browser globals
-        root = root || window;
         root.VS = factory();
     }
 }(this, function () {
