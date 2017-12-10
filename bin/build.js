a29ad4374d26be2574b7a7d18b31a18eae242960
magicuniverse-jay <jay@magicuniverse.nyc>
2017-12-10 00:58:48 -0500

    added eslint config

--- bin/build.js
+++ bin/build.js
@@ -4,8 +4,11 @@ const sass = require('node-sass');
 const uglify = require('uglify-js');
 const fs = require('fs');
 const rimraf = require('rimraf');
+const autoprefixer = require('autoprefixer');
+const postcss = require('postcss');
+const CleanCSS = require('clean-css');
 
-const BASE_DIR = fs.realpathSync(__dirname + '/../') + '/';
+const BASE_DIR = fs.realpathSync(`${__dirname}/../`) + '/';
 const APP_DIR = `${BASE_DIR}app/`;
 const SASS_DIR = `${APP_DIR}styles/`;
 const JS_DIR = `${APP_DIR}scripts/`;
@@ -13,8 +16,7 @@ const BUILD_DIR = `${BASE_DIR}web/`;
 const PROJECT_NAME = 'vinylsiding';
 const PROJECT_VERSION = '1.0';
 
-class Builder
-{
+class Builder {
     start() {
         rimraf(`${BUILD_DIR}css`, (result) => {
             console.log(result);
@@ -41,33 +43,47 @@ class Builder
                 console.error(err);
                 return err;
             }
-            if (!fs.existsSync(write_dir)){
+            if (!fs.existsSync(write_dir)) {
                 fs.mkdirSync(write_dir);
             }
 
-            fs.writeFile(file_path, result.css.toString('utf8'), {}, function (err) {
+            fs.writeFile(file_path, result.css.toString('utf8'), {}, (err) => {
                 if (err) throw err;
                 console.log(`Saved ${file_path}`);
             });
 
-            fs.writeFile(output_map_path, result.map.toString('utf8'), {}, function (err) {
+            fs.writeFile(output_map_path, result.map.toString('utf8'), {}, (err) => {
                 if (err) throw err;
                 console.log(`Saved ${output_map_path}`);
             });
+
+            postcss([ autoprefixer ]).process(result.css).then((result) => {
+                result.warnings().forEach((warn) => {
+                    console.warn(warn.toString());
+                });
+                fs.writeFile(`${write_dir}${file_name}.prefixed.css`, result.css, {}, (err) => {
+                    if (err) throw err;
+                    console.log(`Saved ${write_dir}${file_name}.prefixed.css`);
+                });
+                const minified_css = new CleanCSS({}).minify(result.css);
+                fs.writeFile(`${write_dir}${file_name}.min.css`, minified_css.styles, {}, (err) => {
+                    if (err) throw err;
+                    console.log(`Saved ${write_dir}${file_name}.min.css`);
+                });
+            });
         });
-        // todo: autoprefixer
     }
 
     buildJs() {
         const file_contents = fs.readFileSync(`${JS_DIR}vinylsiding.js`);
         const result = uglify.minify(file_contents, {
             sourceMap: {
-                filename: "out.js",
-                url: "out.js.map"
+                filename: 'out.js',
+                url: 'out.js.map'
             }
         });
         console.log(result.code); // minified output
-        console.log(result.map);  // source map
+        console.log(result.map);
     }
 }
 
