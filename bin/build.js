9564e31ba787a81b713342ee7353c0000fceda3e
magicuniverse-jay <jay@magicuniverse.nyc>
2017-12-09 23:24:30 -0500

    adding build file

--- bin/build.js
+++ bin/build.js
@@ -1,8 +1,14 @@
 'use strict';
 
+const sass = require('node-sass');
+const uglify = require('uglify-js');
+const fs = require('fs');
+
 const APP_DIR = 'app/';
+const SASS_DIR = `${APP_DIR}styles/`;
+const JS_DIR = `${APP_DIR}scripts/`;
 const BUILD_DIR = 'build/';
-const PROJECT_NAME = 'VinylSiding';
+const PROJECT_NAME = 'vinylsiding';
 const PROJECT_VERSION = '1.0';
 
 class Builder
@@ -25,11 +31,29 @@ class Builder
     buildSass() {
         // todo
         // @see https://github.com/sass/node-sass
+        sass.render({
+            file: `${SASS_DIR}vinylsiding.scss`,
+            includeDir: SASS_DIR,
+            outFile: `${BUILD_DIR}/css/${PROJECT_NAME}_${PROJECT_VERSION}.css`,
+            outputStyle: 'compressed',
+            sourceMap: true
+        },
+        function(err, result) {
+            console.log(result)
+        });
         // todo: autoprefixer
     }
 
     buildJs() {
-
+        const file_contents = fs.readFileSync(`${JS_DIR}vinylsiding.js`);
+        const result = uglify.minify(file_contents, {
+            sourceMap: {
+                filename: "out.js",
+                url: "out.js.map"
+            }
+        });
+        console.log(result.code); // minified output
+        console.log(result.map);  // source map
     }
 
     buildAssets() {
