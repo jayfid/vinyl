552b7bcad1b0e98ff563efdb63fab8e650467d47
magicuniverse-jay <jay@magicuniverse.nyc>
2017-12-22 22:58:35 -0500

    added airbnb eslint standard

--- bin/build.js
+++ bin/build.js
@@ -16,20 +16,20 @@ const PROJECT_NAME = 'vinylsiding';
 const PROJECT_VERSION = '1.0';
 
 class Builder {
-    start() {
+    static start() {
         const args = process.argv.slice(2);
         if (args.indexOf('css') !== false) {
             // eslint-disable-next-line no-console
-            console.log('Deleting css directory.');
-            rimraf(`${BUILD_DIR}js`, () => {
-                this.buildSass();
+            console.log(`Deleting directory ${BUILD_DIR}css`);
+            rimraf(`${BUILD_DIR}css`, () => {
+                Builder.buildSass();
             });
         }
         if (args.indexOf('js') !== false) {
             // eslint-disable-next-line no-console
             console.log('Deleting js directory.');
             rimraf(`${BUILD_DIR}js`, () => {
-                this.buildJs();
+                Builder.buildJs();
             });
         }
     }
@@ -40,51 +40,54 @@ class Builder {
         const writeDir = `${BUILD_DIR}css/`;
         const fileName = `${PROJECT_NAME}_${PROJECT_VERSION}`;
         const filePath = `${writeDir}${fileName}.css`;
-        const outputMapPath = `${writeDir}${file_name}.css.map`;
+        const outputMapPath = `${writeDir}${fileName}.css.map`;
+        const prefixedCSSFile = `${writeDir}${fileName}.prefixed.css`;
+        const sourceFile = `${SASS_DIR}vinylsiding.scss`;
+        const SASSoutputStyle = 'expanded';
 
         // eslint-disable-next-line no-console
         console.log('Starting SASS');
         sass.render({
-            file: `${SASS_DIR}vinylsiding.scss`,
+            file: sourceFile,
             includeDir: SASS_DIR,
             outFile: filePath,
-            outputStyle: 'expanded',
+            outputStyle: SASSoutputStyle,
             sourceMap: true,
         }, (err, result) => {
             if (err) throw err;
 
-            if (!fs.existsSync(write_dir)) {
-                fs.mkdirSync(write_dir);
+            if (!fs.existsSync(writeDir)) {
+                fs.mkdirSync(writeDir);
             }
 
-            fs.writeFile(file_path, result.css.toString('utf8'), {}, (err) => {
-                if (err) throw err;
+            fs.writeFile(filePath, result.css.toString('utf8'), {}, (err1) => {
+                if (err1) throw err1;
                 // eslint-disable-next-line no-console
-                console.log(`Saved ${file_path}`);
+                console.log(`Saved ${filePath}`);
             });
 
-            fs.writeFile(output_map_path, result.map.toString('utf8'), {}, (err) => {
-                if (err) throw err;
+            fs.writeFile(outputMapPath, result.map.toString('utf8'), {}, (err2) => {
+                if (err2) throw err2;
                 // eslint-disable-next-line no-console
-                console.log(`Saved ${output_map_path}`);
+                console.log(`Saved ${outputMapPath}`);
             });
 
-            postcss([autoprefixer]).process(result.css).then((result) => {
+            postcss([autoprefixer]).process(result.css).then((postResult) => {
                 result.warnings().forEach((warn) => {
                     // eslint-disable-next-line no-console
                     console.warn(warn.toString());
                 });
-                fs.writeFile(`${write_dir}${file_name}.prefixed.css`, result.css, {}, (err) => {
-                    if (err) throw err;
+                fs.writeFile(prefixedCSSFile, postResult.css, {}, (err3) => {
+                    if (err3) throw err3;
                     // eslint-disable-next-line no-console
-                    console.log(`Saved ${write_dir}${file_name}.prefixed.css`);
+                    console.log(`Saved ${prefixedCSSFile}`);
                 });
-                const minified_css = new CleanCSS({}).minify(result.css);
-                fs.writeFile(`${write_dir}${file_name}.min.css`, minified_css.styles, {}, (err) => {
-                    if (err) throw err;
+                const minifiedCss = new CleanCSS({}).minify(result.css);
+                fs.writeFile(`${writeDir}${fileName}.min.css`, minifiedCss.styles, {}, (err4) => {
+                    if (err4) throw err4;
 
                     // eslint-disable-next-line no-console
-                    console.log(`Saved ${write_dir}${file_name}.min.css`);
+                    console.log(`Saved ${writeDir}${fileName}.min.css`);
                     // eslint-disable-next-line no-console
                     console.log('Finished SASS');
                 });
@@ -92,37 +95,37 @@ class Builder {
         });
     }
 
-    buildJs() {
-        const write_dir = `${BUILD_DIR}js/`;
-        const file_name = `${PROJECT_NAME}_${PROJECT_VERSION}`;
-        const file_path = `${write_dir}${file_name}.js`;
-        const output_map_path = `${file_path}.map`;
+    static buildJs() {
+        const writeDir = `${BUILD_DIR}js/`;
+        const fileName = `${PROJECT_NAME}_${PROJECT_VERSION}`;
+        const filePath = `${writeDir}${fileName}.js`;
+        const outputMapPath = `${filePath}.map`;
 
-        const file_contents = fs.readFileSync(`${JS_DIR}vinylsiding.js`);
+        const fileContents = fs.readFileSync(`${JS_DIR}${PROJECT_NAME}.js`);
         // eslint-disable-next-line no-console
         console.log('Starting JS');
 
-        const result = uglify.minify(file_contents.toString(), {
+        const result = uglify.minify(fileContents.toString(), {
             sourceMap: {
-                filename: file_name,
-                url: `/js/${file_name}.js.map`
-            }
+                filename: fileName,
+                url: `/js/${fileName}.js.map`,
+            },
         });
 
-        if (!fs.existsSync(write_dir)) {
-            fs.mkdirSync(write_dir);
+        if (!fs.existsSync(writeDir)) {
+            fs.mkdirSync(writeDir);
         }
 
-        fs.writeFile(file_path, result.code, {}, (err) => {
+        fs.writeFile(filePath, result.code, {}, (err) => {
             if (err) throw err;
 
             // eslint-disable-next-line no-console
-            console.log(`Saved ${file_path}`);
+            console.log(`Saved ${filePath}`);
 
-            fs.writeFile(output_map_path, result.map, {}, (err) => {
-                if (err) throw err;
+            fs.writeFile(outputMapPath, result.map, {}, (err1) => {
+                if (err1) throw err1;
                 // eslint-disable-next-line no-console
-                console.log(`Saved ${output_map_path}`);
+                console.log(`Saved ${outputMapPath}`);
                 // eslint-disable-next-line no-console
                 console.log('Finished JS');
             });
@@ -130,5 +133,4 @@ class Builder {
     }
 }
 
-const builder = new Builder();
-builder.start();
+Builder.start();
